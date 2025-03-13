eecb48346b8146f4a8136821f9479821fa8443b8
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-03-13 13:09:04 +0000

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Ed's Neovim configuration based on notashelf/nvf";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nvf.url = "github:notashelf/nvf";
+    flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {nixpkgs, ...} @ inputs: {
+    packages.x86_64-linux = {
+      default =
+        (inputs.nvf.lib.neovimConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            {
+              config.vim = {
+                viAlias = true;
+                vimAlias = true;
+                theme = {
+                  enable = true;
+                  name = "catppuccin";
+                  style = "mocha";
+                  transparent = false;
+                };
+                spellcheck.enable = true;
+                statusline = {
+                  lualine = {
+                    enable = true;
+                    theme = "catppuccin";
+                  };
+                };
+              };
+            }
+          ];
+        })
+        .neovim;
+    };
+  };
+}
